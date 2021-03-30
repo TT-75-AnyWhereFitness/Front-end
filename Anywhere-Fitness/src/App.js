775eeb95468d8a65eddb2a63e5049fbe1c3bcb95
hans0 <kiessler.hans@gmail.com>
2021-03-30 19:27:08 -0400

    Need to make Login decision

--- Anywhere-Fitness/src/App.js
+++ Anywhere-Fitness/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route,  Switch,  useHistory, BrowserRouter as Router } from "react-router-dom";
 import axios from "axios"
+
 import LandingPage from "./components/LandingPage"
 function App() {
   return (
     <Router>
       {/* Header goes here */}
       <Switch>
-        <Route exact path='/' component={Login} />
+        {/* Which Login component are we using? */}
+        <Route exact path='/' component={LandingPage} />
         <Route path='/home' component={LandingPage} />
       </Switch>
     </Router>
