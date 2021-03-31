6e3179e8823722628370d2b45bca8eb3bd3994e3
gavin-rilee <you@example.com>
2021-03-31 13:38:00 -0400

    Update2

--- Anywhere-Fitness/src/Signup.js
+++ Anywhere-Fitness/src/Signup.js
@@ -1,105 +1,120 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 const initialState = {
-   fName:"",
-   lName:"",
-   code:"",
-    email:"",
-    password:"",
-   
+  fName: "",
+  lName: "",
+  code: "",
+  email: "",
+  password: "",
+  role: "",
+};
 
-}
-
+const initialData = [];
+const initialDisabled = true;
 
 const Signup = () => {
+  const [userLogIn, setUserLogIn] = useState(initialState);
+  const [disabled, setDisabled] = useState(initialDisabled);
+  const [fitnessData, setFitnessData] = useState(initialData);
 
-    const [userLogIn, setUserLogIn] = useState(initialState);
-    const [disabled, setDisabled] = useState(initialDisabled)
+  const onChange = (evt) => {
+    setUserLogIn({ ...userLogIn, [evt.target.name]: evt.target.value });
+    console.log(userLogIn);
+  };
 
-
-    const onChange = (evt) => {
-        setUserLogIn({...userLogIn, [evt.target.name]:evt.target.value} ) 
-        console.log(userLogIn)
-       }
-
-       const onSubmit = (evt) => {
-        evt.preventDefault();
+  const onSubmit = (evt) => {
+    evt.preventDefault();
     const newUser = {
-        fname: userLogIn.fName,
-        lName: userLogIn.lName,
-        email: userLogIn.email,
-        password: userLogIn.password,
+      option: userLogIn.option,
+      fname: userLogIn.fName,
+      lName: userLogIn.lName,
+      email: userLogIn.email,
+      password: userLogIn.password,
     };
     axios
-    .post("https://reqres.in/api/users", newUser)
-    .then((res) => {
-    //   setOrder(res.data, ...order);
-    //   setCreateForm(initialState);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-    }
+      .post("https://reqres.in/api/users", newUser)
+      .then((res) => {
+        setFitnessData(res.data, ...fitnessData);
+        setUserLogIn(initialState);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return (
-        <>
-            <form>
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <label>
+          Client or Instructor?
+          <select id="role" name="role" onChange={onChange}>
+            <option value="">Choose option here</option>
+            <option value="client">Client</option>
+            <option value="instructor">Instructor</option>
+          </select>
+        </label>
+        <label>
+          First Name:
+          <input
+            name="name"
+            type="text"
+            value={userLogIn.fName}
+            onChange={onChange}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            name="name"
+            type="text"
+            value={userLogIn.lName}
+            onChange={onChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            name="email"
+            type="email"
+            value={userLogIn.email}
+            onChange={onChange}
+          />
+        </label>
+        <label>
+          Create a password:
+          <input
+            name="password"
+            type="password"
+            value={userLogIn.password}
+            onChange={onChange}
+          />
+        </label>
+        <label>
+          Code:
+          <input
+            name="code"
+            type="text"
+            value={userLogIn.code}
+            onChange={onChange}
+          />
+        </label>
+        {
+            userLogIn.role === "client" ? null : 
             <label>
-        Client or Instructor?
-        <select id='option' name='option' onChange={onChange}>
-          <option value="">Choose option here</option>
-          <option value="Client">Client</option>
-          <option value="Instructor">Instructor</option>
-        </select>
-      </label>
-            <label>
-                    First Name:
-                    <input
-                    name="name"
-                    type="text"
-                    value={userLogIn.fName}
-                    onChange={onChange}
-                    />
-                </label>
-                <label>
-                    Last Name:
-                    <input
-                    name="name"
-                    type="text"
-                    value={userLogIn.lName}
-                    onChange={onChange}       
-                    />
-                </label>
-                <label>
-                    Email:
-                    <input
-                    name="email"
-                    type="email"
-                    value={userLogIn.email}
-                    onChange={onChange}
-                    />
-                </label>
-                <label>
-                    Create a password:
-                    <input
-                    name="password"
-                    type="password"
-                    value={userLogIn.password}
-                    onChange={onChange}       
-                    />
-                </label>
-                <label>
-                   Code:
-                    <input
-                    name="code"
-                    type="text"
-                    value={userLogIn.code}
-                    onChange={onChange}
-                    />
-                </label>
-            </form>
-        </>
-    )
-}
+          Code:
+          <input
+            name="code"
+            type="text"
+            value={userLogIn.code}
+            onChange={onChange}
+            />
+            </label>
+        }
+        <button disabled={disabled}>Submit</button>
 
-export default Signup
+      </form>
+    </>
+  );
+};
+
+export default Signup;
