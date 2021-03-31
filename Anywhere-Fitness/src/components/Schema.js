e7d8ac87bc50b3b0d6e2803d303739b13feebbe8
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-03-31 13:23:23 -0500

    Trying to get merge to work

---
+++ Anywhere-Fitness/src/components/Schema.js
@@ -0,0 +1,18 @@
+import * as yup from 'yup'
+
+
+const Schema = yup.object().shape({
+    fName: yup
+    .string()
+    .required("Must include a name.")
+    .min(2, "Must include at least 2 characters."),
+    lName: yup
+    .string()
+    .required("Must include a name.")
+    .min(2, "Must include at least 2 characters."),
+    password: yup 
+    .password(),
+    email:yup
+    .string().email(),
+})
+export default Schema
