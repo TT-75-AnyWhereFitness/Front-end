719f59f4ec81b4db009bcbc05a1804613e12629f
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-03-30 20:05:13 -0500

    Adding axios with Auth, some Routing

--- Anywhere-Fitness/src/App.js
+++ Anywhere-Fitness/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import {  BrowserRouter as Router } from "react-router-dom";
-import LandingPage from "./components/LandingPage"
+import { Route, Switch, useHistory } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
 function App() {
   return (
-    <Router>
+    <div>
       <LandingPage />
-    </Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </div>
   );
 }
 
 export default App;
-// USE LANDING.HTML FOR THIS
